ac8d15ebb84cf846933a8aa3fe3dec5a6ccd0149
Enciclo <enciclo@mail.ru>
2023-02-08 20:46:39 +0300

    Add FlipEffect and Screenshoot

--- js/script.js
+++ js/script.js
@@ -7,7 +7,48 @@ function getWunch(){
             alert('Введённый код не соответсвует формату: 0А00АА0');
             return;
         } 
-    
-    alert(`Ваше пожелание: ${CodeArray[code]}`);
+    front =  document.querySelector('.front');
+    back =  document.querySelector('.back');
+    front.style.transform = 'rotateY(180deg)';
+    back.style.transform = 'rotateY(360deg)';
+    desire_text = CodeArray[code];
+    document.getElementById('desire').value = `"${desire_text}"`;    
        
+}
+
+async function Screenshot(){
+    var constraints = {
+        preferCurrentTab: true,
+        audio: false,
+        video: {
+          width: { ideal: 1920, max: 7680 },
+          height: { ideal: 1080, max: 4320 }
+        }
+      }
+    try {
+        const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+        const video = document.createElement("video");
+
+        video.addEventListener("loadedmetadata", () => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+
+            canvas.height = video.videoHeight;
+            canvas.width = video.videoWidth;
+
+            video.play();
+
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            stream.getVideoTracks()[0].stop();
+            let a = document.createElement("a");
+            a.download = "ss.png";
+            a.href = canvas.toDataURL("image/png");
+            a.click();
+        });
+        video.srcObject = stream;
+        console.log(stream);
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
